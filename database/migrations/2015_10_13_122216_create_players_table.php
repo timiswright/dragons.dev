167a4fcd5f798ce3e6199aece1d23a90a43b39a6
Tim <tim@iMac.local>
2015-10-13 19:38:18 +0100

    migrations created and generators added

---
+++ database/migrations/2015_10_13_122216_create_players_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePlayersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('players', function (Blueprint $table) {
+            $table->increments('id');
+            $table->bigInteger('user_id');
+            $table->bigInteger('team_id');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('gender', 1);
+            $table->string('mobile');
+            $table->integer('rating');
+            $table->string('position');
+            $table->boolean('starred');
+            $table->string('login_hash');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('players');
+    }
+}
---
+++ database/migrations/2015_10_13_141309_create_event_player_pivot_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEventPlayerPivotTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('event_player', function(Blueprint $table) {
+            $table->integer('event_id')->unsigned()->index();
+            $table->foreign('event_id')->references('id')->on('events')->onDelete('cascade');
+            $table->integer('player_id')->unsigned()->index();
+            $table->foreign('player_id')->references('id')->on('players')->onDelete('cascade');
+            $table->string('availability');
+            $table->string('availability_notes');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('event_player');
+    }
+}
